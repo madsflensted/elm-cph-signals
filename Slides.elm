692d3084c855eb843952f38197fd3cff9f3d6038
Mads Flensted-Urech <mfu@coadu.com>
2015-11-23 08:57:31 +0100

    Slide show structre

---
+++ Slides.elm
@@ -0,0 +1,33 @@
+module Slides where
+
+allSlides = 
+  [ """
+# Elm Copenhagen
+Wedensday November 25. at BestBrains
+"""
+  , """
+## Program
+- 18.00 - 19.00, Elm Signals
+- 19.00 - 20.30, ???, group work and solution sharing
+- 20.45 - 21.00, Demo's
+"""
+  , """
+# Signals in core
+- [Keyboard](http://package.elm-lang.org/packages/elm-lang/core/2.1.0/Mouse)
+- [Mouse](http://package.elm-lang.org/packages/elm-lang/core/2.1.0/Mouse)
+- [Time](http://package.elm-lang.org/packages/elm-lang/core/2.1.0/Time)
+- [Touch](http://package.elm-lang.org/packages/elm-lang/core/2.1.0/Touch)
+- [Window](http://package.elm-lang.org/packages/elm-lang/core/2.1.0/Window)
+
+"""
+  , """
+# Signal manipulation
+[Signal](http://package.elm-lang.org/packages/elm-lang/core/2.1.0/Signal)
+- map, map2, map3 ...
+- merge, mergeMany
+- foldp
+- filter, dropRepeats, sampleOn
+
+[Signal Extra](http://package.elm-lang.org/packages/Apanatshka/elm-signal-extra/5.6.0)
+"""
+  ]
